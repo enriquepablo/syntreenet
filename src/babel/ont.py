4949d7f047c96407ed379f24da8e6063f683823e
enriquepablo <enrique@cazalla.net>
2019-07-05 22:28:06 +0200

    license, and syntreenet it is

--- src/babel/ont.py
+++ src/babel/ont.py
@@ -1,14 +1,14 @@
 # Copyright (c) 2019 by Enrique Pérez Arnaud <enrique@cazalla.net>
 #
-# This file is part of the whatever project.
-# https://whatever.cazalla.net
+# This file is part of the syntreenet project.
+# https://syntree.net
 #
-# The whatever project is free software: you can redistribute it and/or modify
+# The syntreenet project is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# The whatever project is distributed in the hope that it will be useful,
+# The syntreenet project is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
@@ -117,7 +117,7 @@ class F(Fact):
                 obj = path.value
         return cls(subj, pred, obj)
 
-# We now impose a logic on this grammar, using duoligno
+# We now impose a logic on this grammar, using syntreenet
 
 
 kb = KnowledgeBase()
--- src/core.py
+++ src/core.py
@@ -1,14 +1,14 @@
 # Copyright (c) 2019 by Enrique Pérez Arnaud <enrique@cazalla.net>
 #
-# This file is part of the whatever project.
-# https://whatever.cazalla.net
+# This file is part of the syntreenet project.
+# https://syntree.net
 #
-# The whatever project is free software: you can redistribute it and/or modify
+# The syntreenet project is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# The whatever project is distributed in the hope that it will be useful,
+# The syntreenet project is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
