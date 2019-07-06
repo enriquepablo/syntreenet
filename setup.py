fb6c35f5ff8d13785a74887bebfe279303c696de
enriquepablo <enrique@cazalla.net>
2019-07-06 19:37:44 +0200

    packaging

--- setup.py
+++ setup.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env python
-import os
-from setuptools import setup, find_packages
+import setuptools
 
 version = '0.1.0a1'
 
-here = os.path.abspath(os.path.dirname(__file__))
 
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
 
-setup(
+setuptools.setup(
     name='syntreenet',
     version=version,
     license='GPLv3',
-    url='http://syntree.net/',
+    url='http://www.syntree.net/',
     author='Enrique Pérez Arnaud',
     author_email='enrique@cazalla.net',
-    description='A library to develop production rule systems',
-    classifiers=[ ],
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    zip_safe=True,
-    include_package_data=True,
+    description='A library to develop scalable production rule systems',
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPLv3",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    include_package_data=True
 )
