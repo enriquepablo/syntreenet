6579cab6092355c68b98f7d6d3ee5e527fec96ca
enriquepablo <enrique@cazalla.net>
2019-07-21 14:31:49 +0200

    recursive grammar with vars ranging over non-terminal recursive symbols

--- src/syntreenet/tests/test_factset.py
+++ src/syntreenet/tests/test_factset.py
@@ -62,3 +62,39 @@ class BoldTextTests(GrammarTestCase):
         var = f2.get_all_paths()[1].value
 
         self.assertEquals(resp[0][var], val)
+
+
+class PairsTests(GrammarTestCase):
+    grammar_file = 'pairs.peg'
+
+    def test_fact(self):
+        tree = self.kb.parse('(hola : adios, hello : bye)')
+        f = g.Fact.from_parse_tree(tree)
+        self.kb.fset.add_fact(f)
+        resp = self.kb.fset.ask_fact(f)
+        self.assertTrue(resp)
+
+    def test_false_fact(self):
+        tree1 = self.kb.parse('(hola : adios, hello : bye)')
+        f1 = g.Fact.from_parse_tree(tree1)
+        tree2 = self.kb.parse('(hola : adios, bye : hello)')
+        f2 = g.Fact.from_parse_tree(tree2)
+        self.kb.fset.add_fact(f1)
+        resp = self.kb.fset.ask_fact(f2)
+        self.assertFalse(resp)
+
+    def test_nested_fact(self):
+        tree = self.kb.parse('(es : (hola : adios), en : (hello : bye))')
+        f = g.Fact.from_parse_tree(tree)
+        self.kb.fset.add_fact(f)
+        resp = self.kb.fset.ask_fact(f)
+        self.assertTrue(resp)
+
+    def test_false_nested_fact(self):
+        tree1 = self.kb.parse('(es : (hola : adios), en : (hello : bye))')
+        f1 = g.Fact.from_parse_tree(tree1)
+        tree2 = self.kb.parse('(es : (hola : adios), en : (bye : hello))')
+        f2 = g.Fact.from_parse_tree(tree2)
+        self.kb.fset.add_fact(f1)
+        resp = self.kb.fset.ask_fact(f2)
+        self.assertFalse(resp)
